343d721ab3d75a462a9ce8f5b7698b8736350ef8
SHINO-01 <sakifhussain33@gmail.com>
2025-02-06 12:41:57 +0600

    `Update generate_TOC.py and mkdocs.yml for project documentation generation`

--- automation/generate_TOC.py
+++ automation/generate_TOC.py
@@ -6,20 +6,25 @@ INDEX_FILE = "docs/index.md"
 def generate_toc():
     toc = ["# Unified Project Documentation\n", "## Table of Contents\n"]
 
-    projects = sorted([d for d in os.listdir(DOCS_DIR) if os.path.isdir(os.path.join(DOCS_DIR, d))])
+    projects = sorted(
+        [d for d in os.listdir(DOCS_DIR) if os.path.isdir(os.path.join(DOCS_DIR, d))]
+    )
 
     for project in projects:
         project_path = os.path.join(DOCS_DIR, project)
         versions = sorted(
             [v for v in os.listdir(project_path) if os.path.isdir(os.path.join(project_path, v))],
-            reverse=True
+            reverse=True  # Newest version first
         )
 
         if versions:
-            latest_version = versions[0]
-            latest_docs_path = f"{DOCS_DIR}/{project}/{latest_version}/docs/index.md"
-            toc.append(f"- **[{project.replace('_', ' ').title()} - {latest_version}]({latest_docs_path})**\n")
+            toc.append(f"- **{project.replace('_', ' ').title()}**\n")
 
+            for version in versions:
+                version_docs_path = f"{DOCS_DIR}/{project}/{version}/docs/index.md"
+                toc.append(f"  - [{version}]({version_docs_path})\n")
+
+    # Write the Table of Contents to the main index file
     with open(INDEX_FILE, "w", encoding="utf-8") as f:
         f.writelines(toc)
 
